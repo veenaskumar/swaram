e64992a01e1c991a03ea0a5d5ef01b0e146254a7
TharunD <tharuntharun7248@gmail.com>
2024-01-11 20:59:52 +0530

    Tharun
    popup and gallerymain

---
+++ src/components/navbar1.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import logo from '../images/logo.png'
+import '../stylesheet/navbar.css'
+
+const Navbar1 = () => {
+    const handleSetActive = (to) => {
+        console.log(to);
+      };
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    const closeMenu = () => setClick(false)
+
+    return (
+        <div className='header'>
+            <nav className='navbar'>
+                <a href='/' className='logo'>
+                    <img src={logo} alt='logo' />
+                </a>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className='nav-item'>
+                        <a href='/' onClick={closeMenu} spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onSetActive={handleSetActive}>Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#about' onClick={closeMenu} spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onSetActive={handleSetActive}>About</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#gallary' onClick={closeMenu} spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onSetActive={handleSetActive}>Gallary</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#ourteam' onClick={closeMenu} spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onSetActive={handleSetActive}>Our Team</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#contact' onClick={closeMenu} spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onSetActive={handleSetActive}>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar1
