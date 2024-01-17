830029e890b772dd73f4257f2c64bac0feec7297
rs-drawz <sabarishr.21it@kongu.edu>
2024-01-17 19:12:30 +0530

    Home Page modified

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-// import './LaunchTimer'
+import '../../stylesheet/home.css';
 import LaunchTimer from "./Home-LaunchTimer";
-import Typewritter from "./Home-Typewriter";
+import TypewriterComponent from "./Home-Typewriter";
+
 
 function Home() {
   return (
     <>
       <div id="home" className="home-SCC min-h-screen">
-        <div className="home-section-Rpp  bg-no-repeat  bg-center bg-fixed flex items-center w-screen bg-contain">
-          <div className="auto-group-rk2v-NBe relative h-screen ">
+        <div className="home-section-Rpp bg-fixed">
+          <div className="auto-group-rk2v-NBe ">
             <img
-              className="rectangle-2-hji absolute w-96.3 h-full  bg-contain"
+              className="rectangle-2-hji "
               src="./Home-img/rectangle-2.png"
               alt="Rectangle 2"
             />
-            <div className="booknow-crg absolute left-11.1 top-20.1 w-19.9 h-6.1 flex items-center  text-2.9 font-bold text-white border-2 border-fuchsia-500 rounded-md">
-              <button>BOOK NOW</button>
+            <div className="border border-gradient p-3 text-white transition duration-300 hover:bg-gradient hover:border-gradient rs">
+              BOOK NOW
             </div>
-            <p className="music-event-qDe absolute left-11.1 top-13.5 w-66.5 h-13.7 text-10 font-bold text-white animate-multicolor-change">
-              <Typewritter/>
+            <p className="music-event-qDe absolute ">
+            <TypewriterComponent/>
             </p>
             <p className="crazy-non-stop-music-jpp absolute left-11.1 top-27.2 w-35.8 h-4.8 text-3.5 font-semibold text-white capitalize">
               Crazy Non stop Music
@@ -30,13 +31,13 @@ function Home() {
                   src="./Home-img/vector.png"
                   alt="Vector"
                 />
-                <p className="erode-qFa text-3 font-semibold text-white capitalize">
+                <p className="erode-qFa text-3 ">
                   Erode
                 </p>
               </div>
               <div className="group-1-WFE flex items-center">
                 <img
-                  className="vector-esE mr-1 w-3.2 h-2.9"
+                  className="vector-esE "
                   src="./Home-img/vector-UvQ.png"
                   alt="Vector"
                 />
@@ -45,12 +46,9 @@ function Home() {
                 </p>
               </div>
             </div>
-            <div
-  style={{ width: "600px", height: "60px" }}
-  className="timer-VN4 absolute left-84.5 top-39.4 flex items-center justify-between"
->
-  <LaunchTimer />
-</div>
+            <div className="timer-VN4 ">
+              <LaunchTimer />
+            </div>
 
           </div>
         </div>
