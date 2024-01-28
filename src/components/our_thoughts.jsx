2953218cfb83298732aa3ecdf5ee1c1f0e73f633
Veenas kumar <venaskumar812@gmail.com>
2024-01-29 00:11:21 +0530

    out thoughts section added

--- src/components/our_thoughts.jsx
+++ src/components/our_thoughts.jsx
@@ -3,39 +3,6 @@ import Zoom from 'react-reveal/Zoom'
 function Our_thoughts() {
     return (
         <Zoom>
-            {/* <div className='flex flex-col gap-[3rem] justify-center items-center ' >
-        <div>
-            <h1 className='text-[red]'>Our thoughts</h1>
-        </div>
-        <div className='row gap-[5rem] justify-around items-center gap-y-[5rem]'>
-            <div className='w-[400px] gap-[3rem] justify-between  bg-[white] rounded-3xl p-[20px]'>
-                
-                    <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" alt="" height={"100px"} width={"100px"} className='rounded-full h-[100px] float-left p-[5px]'  />
-              
-               
-                    <p className=' text-black  text-justify text-balance col pl-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores hic magnam necessitatibus, repellendus temporibus nulla. Explicabo quibusdam praesentium aut dolorum officia atque. Non architecto in ea, at facilis labore necessitatibus doloribus tenetur mollitia saepe doloremque recusandae deserunt? Dolorem, officiis at.</p>
-                
-            </div>
-            <div className='w-[400px] gap-[3rem] justify-between  bg-[white] rounded-3xl p-[20px]'>
-                
-                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" alt="" height={"100px"} width={"100px"} className='rounded-full h-[100px] float-left p-[5px]'  />
-                <p className='text-black'>president</p>
-          
-           
-                <p className=' text-black  text-justify text-balance col pl-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores hic magnam necessitatibus, repellendus temporibus nulla. Explicabo quibusdam praesentium aut dolorum officia atque. Non architecto in ea, at facilis labore necessitatibus doloribus tenetur mollitia saepe doloremque recusandae deserunt? Dolorem, officiis at.</p>
-            
-        </div>
-        <div className='w-[400px] gap-[3rem] justify-between  bg-[white] rounded-3xl p-[20px]'>
-                
-                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" alt="" height={"100px"} width={"100px"} className='rounded-full h-[100px] float-left p-[5px]'  />
-          
-           
-                <p className=' text-black  text-justify text-balance col pl-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores hic magnam necessitatibus, repellendus temporibus nulla. Explicabo quibusdam praesentium aut dolorum officia atque. Non architecto in ea, at facilis labore necessitatibus doloribus tenetur mollitia saepe doloremque recusandae deserunt? Dolorem, officiis at.</p>
-            
-        </div>
-        </div>
-
-    </div> */}
             <div class=" flex flex-col items-center justify-center ">
                 <div>
                     <h1 className='text-[red] text-center font-semibold'>Our Thoughts</h1>
