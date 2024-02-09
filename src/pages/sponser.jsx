3a264dbbe8860f5658af2afdb2b0c7371f5db3e3
Veenas kumar <venaskumar812@gmail.com>
2024-02-09 11:29:55 +0530

    sponser template is added

--- src/pages/sponser.jsx
+++ src/pages/sponser.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import Navbar1 from '../components/navbar'
+import Contact from '../components/contact'
 function Sponser() {
   return (
-    <>
-        <Navbar1 />
-    </>
+    <div className='w-[100% ] flex flex-col gap-[2rem]'>
+      <Navbar1 />
+      <div className='mt-[100px] flex flex-col gap-[5rem] px-[7%]'>
+        <div className='flex items-center justify-center flex-col gap-[2rem] '>
+          <h1 className='text-[red]'>Sponser</h1>
+          <img src="https://t4.ftcdn.net/jpg/00/90/16/51/360_F_90165155_MCR2C1DvJnu7cJ8hGcGTkwDqyApPdH9q.jpg" alt="" height={"400px"} width={"400px"} />
+        </div>
+        <div className='flex justify-between '>
+          <div>
+            <img src="https://t4.ftcdn.net/jpg/00/90/16/51/360_F_90165155_MCR2C1DvJnu7cJ8hGcGTkwDqyApPdH9q.jpg" alt="" height={"300px"} width={"300px"} />
+          </div>
+          <div>
+            <img src="https://t4.ftcdn.net/jpg/00/90/16/51/360_F_90165155_MCR2C1DvJnu7cJ8hGcGTkwDqyApPdH9q.jpg" alt="" height={"300px"} width={"300px"} />
+          </div>
+        </div>
+        <div className='row justify-center gap-[3rem] items-center gap-y-[4rem] '>
+          <div>
+            <img src="https://www.shutterstock.com/image-vector/sponsor-banner-template-ribbon-label-260nw-1725400003.jpg" alt="" height={"250px"} width={"250px"} />
+          </div>
+          <div>
+            <img src="https://www.shutterstock.com/image-vector/sponsor-banner-template-ribbon-label-260nw-1725400003.jpg" alt="" height={"250px"} width={"250px"} />
+            </div>
+          <div>
+            <img src="https://www.shutterstock.com/image-vector/sponsor-banner-template-ribbon-label-260nw-1725400003.jpg" alt="" height={"250px"} width={"250px"} />
+            </div>
+            <div>
+            <img src="https://www.shutterstock.com/image-vector/sponsor-banner-template-ribbon-label-260nw-1725400003.jpg" alt="" height={"250px"} width={"250px"} />
+            </div>
+            <div>
+            <img src="https://www.shutterstock.com/image-vector/sponsor-banner-template-ribbon-label-260nw-1725400003.jpg" alt="" height={"250px"} width={"250px"} />
+            </div>
+        </div>
+      </div>
+      <Contact />
+    </div>
   )
 }
 
